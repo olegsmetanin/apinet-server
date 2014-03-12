f4a9d507f64b7fde6c18973bfb4f7c02571c55e1
Artem Naumov <naumov.artem@gmail.com>
2014-03-12 10:27:56 +0400

    Issue refs #1766: done

--- AGO.Tasks/SecurityProviders/TaskSecurityProvider.cs
+++ AGO.Tasks/SecurityProviders/TaskSecurityProvider.cs
@@ -1,4 +1,5 @@
-﻿using AGO.Core.Filters;
+﻿using System.Linq;
+using AGO.Core.Filters;
 using AGO.Core.Model.Projects;
 using AGO.Core.Model.Security;
 using AGO.Tasks.Model.Task;
@@ -14,8 +15,16 @@ namespace AGO.Tasks.SecurityProviders
 
 		public override IModelFilterNode ReadConstraint(string project, ProjectMemberModel member, ISession session)
 		{
-			if (member.ProjectCode == project) return null;
-			return FilteringService.Filter<TaskModel>().Where(m => 1 == 2);
+			//not a members can't see project data
+			if (member.ProjectCode != project) return FilteringService.Filter<TaskModel>().Where(m => 1 == 2);
+			//executors can see only his tasks
+			if (member.IsInRole(TaskProjectRoles.Executor))
+				return FilteringService.Filter<TaskModel>()
+					.WhereCollection(m => m.Executors)
+					.Where(m => m.Executor.Id == member.Id)
+					.End();
+			//other roles (admins and managers) can see all tasks
+			return null;
 		}
 
 		public override bool CanCreate(TaskModel model, string project, ProjectMemberModel member, ISession session)
@@ -25,7 +34,11 @@ namespace AGO.Tasks.SecurityProviders
 
 		public override bool CanUpdate(TaskModel model, string project, ProjectMemberModel member, ISession session)
 		{
-			return true;//any member can change task
+			//executors can change own tasks
+			if (member.IsInRole(TaskProjectRoles.Executor))
+				return model.Executors.Any(e => e.Executor.Equals(member));
+			//admins and manager can change any task
+			return true;
 		}
 
 		public override bool CanDelete(TaskModel model, string project, ProjectMemberModel member, ISession session)
